a4ac07306a52d5062fc5e68771948c2317de8b0f
Brian Egan <brian.egan@unlv.edu>
2009-10-17 22:22:39 -0700

    Fixed two errors reported by JavaScript Lint

--- scripts/dmcarousel.js
+++ scripts/dmcarousel.js
@@ -8,7 +8,7 @@
  *  http://www.opensource.org/licenses/mit-license.php
  * 
  **************************************/
-
+ 
 ;(function($) {
 // Creates a pretty carousel (slideshow)
 $.fn.dmCarousel = function(options) {
@@ -21,7 +21,7 @@ $.fn.dmCarousel = function(options) {
       // Support for the Metadata Plugin.
       o = $.meta ? $.extend({}, opts, $this.data()) : opts,
       db = true,
-      carousel = { slide: [] }, // = JSON.parse('{"query":{"collections":["\/snv"]},"resultsPage":1,"resultsPerPage":20,"numPages":83,"onThisPage":20,"total":1650,"results":[{"alias":"\/snv","ptr":"6290"},{"alias":"\/snv","ptr":"4868"},{"alias":"\/snv","ptr":"2678"},{"alias":"\/snv","ptr":"4838"},{"alias":"\/snv","ptr":"4827"},{"alias":"\/snv","ptr":"4731"},{"alias":"\/snv","ptr":"4834"},{"alias":"\/snv","ptr":"4729"},{"alias":"\/snv","ptr":"4812"},{"alias":"\/snv","ptr":"6603"},{"alias":"\/snv","ptr":"2539"},{"alias":"\/snv","ptr":"1173"},{"alias":"\/snv","ptr":"1321"},{"alias":"\/snv","ptr":"1313"},{"alias":"\/snv","ptr":"2561"},{"alias":"\/snv","ptr":"9"},{"alias":"\/snv","ptr":"204"},{"alias":"\/snv","ptr":"215"},{"alias":"\/snv","ptr":"130"},{"alias":"\/snv","ptr":"230"}],"querySeconds":0.39331,"version":"1.0"}');
+      carousel = { slide: [] },
       
       init = function() {
         
@@ -41,7 +41,7 @@ $.fn.dmCarousel = function(options) {
             if (db) debug("Error: " + textStatus);
           }
         });
-      }
+      },
       
       buildCarousel = function() {
         
